7e922debc145ff920253045ae1454e453962b9ad
Adhil Ansari <frkadhilansaripv@gmail.com>
2023-05-30 15:43:13 +0530

    updated

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 import express from "express"
-// import cors from "cors"
+import cors from "cors"
 import foodRouter from './routers/food.router'
 import userRouter from "./routers/user.router";
 import orderRouter from "./routers/order.router";
@@ -17,10 +17,11 @@ const app =express();
 app.use(express.json());
 app.use(express.static('public'))
 
-// app.use(cors({
-//     credentials:true,
-//     origin:["http://localhost:4200"]
-// }));
+app.use(cors({
+    credentials:true,
+    // origin:["http://localhost:4200"]
+    origin:['*']
+}));
 
 app.use("/api/foods",foodRouter);
 app.use("/api/users", userRouter);
